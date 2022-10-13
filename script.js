9aa276b2b49c67d7772d99bfe190d4b3c54f0e29
Max <max.arnouts@ictmbo.nl>
2022-10-13 09:51:39 +0200

    Javascript update

--- script.js
+++ script.js
@@ -1,88 +1,87 @@
 // Navbar begin
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
+    const homeLogo = document.getElementById('homeLogo');
+    const hamburgerSwitch = document.getElementById('hamburgerSwitch');
 
-const hamburgerMenu = document.getElementById('hamburgerMenu');
-        const homeLogo = document.getElementById('homeLogo');
-        const hamburgerSwitch = document.getElementById('hamburgerSwitch');
+    const closeSign = document.getAnimations('closeSign');
+    const openSign = document.getAnimations('openSign');
 
-        const closeSign = document.getAnimations('closeSign');
-        const openSign = document.getAnimations('openSign');
-
-        const closeOnClick = document.getElementById('closeOnClick');
+    const closeOnClick = document.getElementById('closeOnClick');
 
 
-        hamburgerSwitch.className = 'close'
+    hamburgerSwitch.className = 'close'
 
-        function openHamburger() {
-            if (hamburgerSwitch.className = 'close') {
-                hamburgerSwitch.className = 'open'
-            }
-
-            if (hamburgerSwitch.className = 'open') {
-                document.getElementById('openSign').innerHTML = `
-                <li class="nav-logo">
-                    <button onclick="closeHamburger()" >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </li>`
-            }
-        }
-        function closeHamburger() {
-            if (hamburgerSwitch.className = 'open') {
-                hamburgerSwitch.className = 'close'
-            }
-            if (hamburgerSwitch.className = 'close') {
-                document.getElementById('openSign').innerHTML = `
-                <li class="nav-logo">
-                    <button onclick="openHamburger()" >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    </button>
-                </li>`
-            }
+    function openHamburger() {
+        if (hamburgerSwitch.className = 'close') {
+            hamburgerSwitch.className = 'open'
         }
 
-        // onclick mobile navbar will close.
-        closeOnClick.addEventListener("click", function(event) {
-            if (hamburgerSwitch.className = 'open') {
-                hamburgerSwitch.className = 'close'
+        if (hamburgerSwitch.className = 'open') {
+            document.getElementById('openSign').innerHTML = `
+            <li class="nav-logo">
+                <button onclick="closeHamburger()" >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </li>`
+        }
+    }
+    function closeHamburger() {
+        if (hamburgerSwitch.className = 'open') {
+            hamburgerSwitch.className = 'close'
+        }
+        if (hamburgerSwitch.className = 'close') {
+            document.getElementById('openSign').innerHTML = `
+            <li class="nav-logo">
+                <button onclick="openHamburger()" >
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+            </li>`
+        }
+    }
 
-                document.getElementById('openSign').innerHTML = `
-                <li class="nav-logo">
-                    <button onclick="openHamburger()" >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    </button>
-                </li>`
+    // onclick mobile navbar will close.
+    closeOnClick.addEventListener("click", function(event) {
+        if (hamburgerSwitch.className = 'open') {
+            hamburgerSwitch.className = 'close'
 
-                closeSign.className = ''
-                openSign.className = 'hide'
-            }
-        })
+            document.getElementById('openSign').innerHTML = `
+            <li class="nav-logo">
+                <button onclick="openHamburger()" >
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+            </li>`
 
-        // Check width size if mobile or windows is.
-        homeLogo.className = '';
+            closeSign.className = ''
+            openSign.className = 'hide'
+        }
+    })
 
-        window.addEventListener("resize", function(event) {
-            viewportwidth = document.body.clientWidth
+    // Check width size if mobile or windows is.
+    homeLogo.className = '';
 
-            if (viewportwidth <= 800) {
-                console.log("Mobile View")
-                hamburgerMenu.className = '';
-                homeLogo.className = 'hide';
+    window.addEventListener("resize", function(event) {
+        viewportwidth = document.body.clientWidth
 
-            } else if (viewportwidth >= 800) {
-                console.log("Window View")
-                hamburgerMenu.className = 'hide';
-                homeLogo.className = '';
-            }
-        })
+        if (viewportwidth <= 800) {
+            console.log("Mobile View")
+            hamburgerMenu.className = '';
+            homeLogo.className = 'hide';
 
-// Navbar end
-// Portfolio begin
+        } else if (viewportwidth >= 800) {
+            console.log("Window View")
+            hamburgerMenu.className = 'hide';
+            homeLogo.className = '';
+        }
+    })
+
+    // Navbar end
+    // Portfolio begin
 
     var buttonClass = document.getElementById("buttonClass")
     var buttonsActive = buttonClass.getElementsByClassName("buttonSwitch");
@@ -110,4 +109,3 @@ const hamburgerMenu = document.getElementById('hamburgerMenu');
         }
     }
 // Portfolio end
-
